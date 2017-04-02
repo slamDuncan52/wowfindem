77015ab514c577e03aad394dd91d45da02deb47e
Mitchell Duncan <dunc0474@kettering.edu>
2017-04-02 00:59:39 -0400

    trying loot event instead

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
-local Congrats_EventFrame = CreateFrame("Frame")
-Congrats_EventFrame:RegsiterEvent("PLAYER_LEVEL_UP")
-Congrats_EventFrame:SetScript("OnEvent", function(self, event, ...) 
-        local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
-          print(Congratulations on reaching level ' ..arg1.. ', ' ..UnitName("Player").. '! You gained ' ..arg2.. ' HP and ' ..arg3.. ' MP!')
+local Loot_EventFrame = CreateFrame("Frame")
+Loot_EventFrame:RegsiterEvent("LOOT_OPENED")
+Loot_EventFrame:SetScript("OnEvent", function(self, event, ...) 
+        local arg1 = ...
+          print("You looted! Your autoloot is set to ' ..arg1.. '")
             end)
