79db5e3ce1e3b099cbfb65a8f6ee7cee7974092b
Mitchell Duncan <dunc0474@kettering.edu>
2017-04-02 01:08:00 -0400

    I can't spell anything

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local Loot_EventFrame = CreateFrame("Frame")
-Loot_EventFrame:RegsiterEvent("LOOT_OPENED")
+Loot_EventFrame:RegisterEvent("LOOT_OPENED")
 Loot_EventFrame:SetScript("OnEvent", function(self, event, ...) 
         local arg1 = ...
           print("You looted! Your autoloot is set to ' ..arg1.. '")
